47468b8dcf29174a5d2c672fee27aedcb325ab6a
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-19 11:31:10 +0530

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -13,8 +13,9 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "--"
-    OWNER_ID = "Ha Ha"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "legendx22"
+    OWNER_ID = "YOUR OWN ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "YOUR USERNAME HERE"
+   
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
