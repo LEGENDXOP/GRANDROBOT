6574af26b6fe72b44c239f72d08da6c57e6b5f70
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-19 12:52:07 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def get_user_list(config, key):
-    with open('{}/cinderella/{}'.format(os.getcwd(), config), 'r') as json_file:
+    with open('{}/tg_bot/{}'.format(os.getcwd(), config), 'r') as json_file:
         return json.load(json_file)[key]
 
 
