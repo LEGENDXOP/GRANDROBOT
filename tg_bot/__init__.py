9e18889dcfa7b33157f5f92bf3f303ba19513b47
LEGEND X <74249238+legendx22@users.noreply.github.com>
2020-12-19 12:57:51 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -173,8 +173,7 @@ else:
         spam_watch = None
         
 # Load at end to ensure all prev variables have been set
-from tg_bot
-.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
+from tg_bot.modules.helper_funcs.handlers import CustomCommandHandler, CustomRegexHandler, CustomMessageHandler
 
 # make sure the regex handler can take extra kwargs
 tg.RegexHandler = CustomRegexHandler
