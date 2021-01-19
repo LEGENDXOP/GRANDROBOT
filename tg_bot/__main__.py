ee63f13987c77171c96e5f85d94ef48cd3bfc98e
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-01-19 20:32:43 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -150,7 +150,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Yuss, I am Already ONline")
+        update.effective_message.reply_text("zinda hu mai bol kya kaam hai")
 
 
 def send_start(bot, update):
