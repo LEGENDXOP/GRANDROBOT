f239b7ca94395bee644457491d14444f56527cda
CrackHack10 <77793128+CrackHack10@users.noreply.github.com>
2021-04-06 22:34:29 +0530

    Main

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,7 +142,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/MrCracker")],
+                                                                       url="t.me/MrCracker10")],
                                                                                    [InlineKeyboardButton(text="ADD Lisa😘 TO YOUR GROUP TO FEEL THE LOVE😍😍",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
