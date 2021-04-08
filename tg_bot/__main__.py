1b30a64fa3a3428459ebacdec5119acd1dc81378
sahilsi478 <80320425+sahilsi478@users.noreply.github.com>
2021-04-08 05:58:49 +0530

    Update_main_py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -142,15 +142,15 @@ def start(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
                                                                                    InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
+                                                                       url="t.me/@legend_s_ravna")],
+                                                                                   [InlineKeyboardButton(text="ADD @D3vil_legend_ravna_bot TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
                                                                        url="https://github.com/legendx22/GRANDROBOT")
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Yuss, I am Already ONline")
+        update.effective_message.reply_text("Yuss, I am Already ONline sir 😁😁")
 
 
 def send_start(bot, update):
