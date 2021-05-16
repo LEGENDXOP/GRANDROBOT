c2eeca0c9d9386fc155773918785f82f8e8d20f7
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-16 18:40:56 +0530

    Create game.py

---
+++ tg_bot/modules/game.py
@@ -0,0 +1,29 @@
+"""
+MADE BY LEGENDX22 🔥
+THIS IS ADULT GAME BOT
+BUT THIS IS NOW NOT READY
+PLEASE WAIT WE ADDED YOU ON DATABSE
+WHEN THIS GAME IS COMPLETED I WILL SEND YOU A MESSAGE
+IF YOU WANT NOT PLAY THIS GAME THEN TYPE `/terminate`
+OK BRO BYE 🤠🤠🤠
+"""
+from telethon import events
+from tg_bot.data.users_db import new_user, already_user, rem_user
+@bot.on(events.NewMessage(pattern="/register"))
+async def pro(event):
+  if already_user(event.sender_id):
+    await event.reply("YOU ARE ALREADY REGISTERD")
+  elif not already_user(event.sender_id):
+    new_user(event.sender_id)
+    await event.reply(__doc__)
+  else:
+   pass
+@bot.on(events.NewMessage(pattern="/terminate"))
+async def terminate (event):
+  if not already_user(event.sender_id):
+    await event.reply("YOU ARE NOT IN THE GAME USE /register FOR REGISTERING")
+  elif already_user(event.sender_id):
+    rem_user(event.sender_id)
+    await event.edit("DONE WE REMOVED YOU, NOW YOU NOT IN GAME")
+  else:
+    pass
