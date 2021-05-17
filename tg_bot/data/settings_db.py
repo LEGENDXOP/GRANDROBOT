52be10bf9aecdf1a0d3309f220aa56000f83369a
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-05-17 10:09:33 +0530

    Create settings_db.py

---
+++ tg_bot/data/settings_db.py
@@ -0,0 +1,24 @@
+# MADE BY LEGENDX22
+# COPY WITH CREDITS
+from .import db
+X = db["settings"]
+def add_name(name):
+  LEGENDX = {
+    "_id": "LEGENDX22"
+    }
+  pro = X.find_one(LEGENDX)
+  if pro:
+    LEGEND = {"_id": "LEGENDX22"}, {"$set": {"username": name}}
+    X.update_one(LEGEND)
+  else:
+    LEGENDXOP = {
+      "_id": "LEGENDX22",
+      "username": name
+      }
+    X.insert_one(LEGENDXOP)
+def give_name():
+  x = X.find_one({"_id": "LEGENDX22"})
+  if x:
+    return x["name"]
+  else:
+    return "LEGENDX22"
