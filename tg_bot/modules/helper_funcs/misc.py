bd6c766a01f8a74faaf5db1109e4daf93bade9eb
DROOPY-4U <81871527+DROOPY-4U@users.noreply.github.com>
2021-07-17 15:18:44 +0700

    Update misc.py

--- tg_bot/modules/helper_funcs/misc.py
+++ tg_bot/modules/helper_funcs/misc.py
@@ -68,9 +68,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 10:10 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("◀️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("⏹", callback_data="bot_start"),
-             EqInlineKeyboardButton("▶️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("👉", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("⛔CLOSE", callback_data="bot_start"),
+             EqInlineKeyboardButton("👈", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
