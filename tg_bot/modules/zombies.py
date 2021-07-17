ecd73a8382427ebdb1b78feeb41c7fd2473fc24e
DROOPY-4U <81871527+DROOPY-4U@users.noreply.github.com>
2021-07-17 11:57:37 +0700

    Update zombies.py

--- tg_bot/modules/zombies.py
+++ tg_bot/modules/zombies.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from LEGENDXOP import telethn, OWNER_ID, DEV_USERS, SUDO_USERS, WHITELIST_USERS
+from tg_bot import telethn, OWNER_ID, DEV_USERS, SUDO_USERS, WHITELIST_USERS
 
 # =================== CONSTANT ===================
 
